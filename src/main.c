024b94ea033f52ae1bcd4d08b16e59da3fd63c94
brentru <brent@adafruit.com>
2023-03-22 12:36:31 -0400

    trying to trace better

--- src/main.c
+++ src/main.c
@@ -12,27 +12,31 @@
 #include "lvgl.h"
 #include <stdio.h>
 
-extern lv_font_t turtle2;
+/**********************
+ *      MACROS
+ **********************/
 #define SYMBOL_TURTLE "\xEF\x9C\xA6"
-
-extern lv_font_t file_code;
 #define SYMBOL_CODE "\xEF\x87\x89"
-
-extern lv_font_t wifi_30px;
 #define SYMBOL_WIFI "\xEF\x87\xAB"
-
-extern lv_font_t turtle_30px;
 #define SYMBOL_TURTLE30PX "\xEF\x9C\xA6"
-
-extern lv_font_t cloud_30px;
 #define SYMBOL_CLOUD "\xEF\x83\x82"
-
-extern lv_font_t circle_30px;
 #define SYMBOL_CHECKMARK "\xEF\x81\x98"
-
-extern lv_font_t errorTriangle;
 #define SYMBOL_ERROR_TRIANGLE "\xEF\x81\xB1"
 
+/**********************
+ *      FONTS
+ **********************/
+extern lv_font_t turtle2;
+extern lv_font_t file_code;
+extern lv_font_t wifi_30px;
+extern lv_font_t turtle_30px;
+extern lv_font_t cloud_30px;
+extern lv_font_t circle_30px;
+extern lv_font_t errorTriangle;
+
+/**********************
+ *  IMAGE DECLARE
+ **********************/
 LV_IMG_DECLARE(ws_icon_100px);
 LV_IMG_DECLARE(ws_logo_200px);
 
@@ -43,35 +47,61 @@ LV_IMG_DECLARE(ws_logo_200px);
   "drop the secrets.json file to the WIPPER drive.\n3. Press RESET on your "   \
   "board."
 
-
-lv_timer_t* splashTask;
+lv_timer_t *splashTask;
 lv_obj_t *splashImg;
 
 lv_obj_t *labelIconFile, *labelWiFi;
 
 // Styles used by the loading screen, need to be global or static
-static lv_style_t styleIconFile, styleIconWiFi, styleIconTurtle30px, styleIconCloud, styleIconCheckmark;
+static lv_style_t styleIconFile, styleIconWiFi, styleIconTurtle30px,
+    styleIconCloud, styleIconCheckmark;
 
 // Icon bar offset and spacing
 const lv_coord_t iconBarXStart = 28;
 const lv_coord_t iconBarYOffset = -45; // gives us room for text, too
 const int iconBarXSpaces = 33;         // +10 exactly between icons
 
-void make_wifi_green(lv_timer_t * timer) {
-    lv_style_set_text_color(&styleIconWiFi, lv_palette_main(LV_PALETTE_GREEN));
-    lv_obj_add_style(labelWiFi, &styleIconWiFi, LV_PART_MAIN);
-    lv_timer_del(timer);
+// mock WS functions
+void provision() { 
+  printf("in provision()\n");
+  // _fileSystem = new Wippersnapper_FS(); // TODO: We'll want to mock this! 
+  // _fileSystem->parseSecrets();
 }
 
-void make_file_green(lv_timer_t * timer) {
-    lv_style_set_text_color(&styleIconFile, lv_palette_main(LV_PALETTE_GREEN));
-    lv_obj_add_style(labelIconFile, &styleIconFile, LV_PART_MAIN);
-    lv_timer_del(timer);
 
-    lv_timer_create(make_wifi_green, 1000, NULL);
+void make_wifi_green(lv_timer_t *timer) {
+  lv_style_set_text_color(&styleIconWiFi, lv_palette_main(LV_PALETTE_GREEN));
+  lv_obj_add_style(labelWiFi, &styleIconWiFi, LV_PART_MAIN);
+  lv_timer_del(timer);
 }
 
-void load_task(lv_timer_t * timer) {
+void make_file_green(lv_timer_t *timer) {
+  lv_style_set_text_color(&styleIconFile, lv_palette_main(LV_PALETTE_GREEN));
+  lv_obj_add_style(labelIconFile, &styleIconFile, LV_PART_MAIN);
+  lv_timer_del(timer);
+
+  lv_timer_create(make_wifi_green, 1000, NULL);
+}
+
+void make_file_green_polling() {
+  lv_style_set_text_color(&styleIconFile, lv_palette_main(LV_PALETTE_GREEN));
+  lv_obj_add_style(labelIconFile, &styleIconFile, LV_PART_MAIN);
+  lv_task_handler();
+}
+
+void checkSecretsFile() {
+  bool fileExists = true;
+  // TODO: need checks for wifi and io key too
+  lv_log("BR: Checking secrets file...");
+  if (fileExists == true) {
+    make_file_green_polling();
+    return;
+    lv_log("returned to secrets file...");
+  }
+}
+
+void load_task(lv_timer_t *timer) {
+  printf("in load_task");
   // remove the splash screen (TODO: do this in another func.)
   lv_obj_del(splashImg);
 
@@ -138,32 +168,36 @@ void load_task(lv_timer_t * timer) {
   lv_timer_del(timer);
 
   // simulate event loop
-  lv_timer_create(make_file_green, 2500, NULL);
+  provision();
 }
 
-void splash_task(lv_timer_t * timer) {
-  // create and load splash screen 
-  
+
+// create and load splash screen
+void splash_task(lv_timer_t *timer) {
+  printf("in splash_task()");
   // create and center the full logo (200px
   splashImg = lv_img_create(lv_scr_act());
   lv_img_set_src(splashImg, &ws_logo_200px);
   lv_obj_align(splashImg, LV_ALIGN_CENTER, 0, 0);
 
-  lv_timer_del(timer); // one-shot task, delete
+  // one-shot task, delete
+  lv_timer_del(timer);
 
   // add load_task to timer handler
   lv_timer_create(load_task, 2500, NULL);
+  printf("done with splash_task()");
 }
-
 
 int main(void) {
   lv_init();
   hal_setup();
 
   lv_obj_set_style_bg_color(lv_scr_act(), lv_color_black(), LV_STATE_DEFAULT);
-  
-  splashTask = lv_timer_create(splash_task, 0, NULL);
 
-  // HAL loop
+  printf("add splash_task\n");
+  splashTask = lv_timer_create(splash_task, 0, NULL);
+  lv_task_handler();
+
+  printf("going into hal_loop...\n");
   hal_loop();
 }
