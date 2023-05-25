dfc451ab5af8593429f0789117fc8a8f108bcf47
brentru <brent@adafruit.com>
2023-05-25 12:59:28 -0400

    scrolling nicely!

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ LV_FONT_DECLARE(cloud_30px);
 static lv_obj_t * consoleLabel;
 
 
-#define MAX_TEXT_BUFFER_SZ 512
+#define MAX_TEXT_BUFFER_SZ 215
 static char textBuffer[MAX_TEXT_BUFFER_SZ + 1]; // + '\0'
 
 #define DISPLAY_WIDTH 240
@@ -38,52 +38,49 @@ int i = 0;
 // Referenced: https://github.com/lvgl/lv_demos/blob/release/v6/lv_apps/terminal/terminal.c
 void monitor_add(const char * txt_in)
 {
-    // size of text passed into this function
-    size_t input_txt_len = strlen(txt_in);
-    // size of textBuffer as it currently stands
-    size_t prv_buffer_len = strlen(textBuffer);
+    //if(win == NULL) return;                 /*Check if the window is exists*/
 
-    // TODO
-    // is the data longer than the label's width?
-    /*     if (input_txt_len > DISPLAY_WIDTH) {
-      // trim the input text down to DISPLAY_WIDTH
-      // modify the buffer to show "..." at the end instead
-      // re-calculate input_text_len with the new txt_in
-      input_txt_len = strlen(txt_in);
-    } */
+    size_t txt_len = strlen(txt_in);
+    size_t old_len = strlen(textBuffer);
 
-    // Check if the new buffer size will exceed MAX_TEXT_BUFFER_SZ
-    if(prv_buffer_len + input_txt_len > MAX_TEXT_BUFFER_SZ) {
+    /*If the data is longer then the terminal ax size show the last part of data*/
+    if(txt_len > MAX_TEXT_BUFFER_SZ) {
+        txt_in += (txt_len - MAX_TEXT_BUFFER_SZ);
+        txt_len = MAX_TEXT_BUFFER_SZ;
+        old_len = 0;
+    }
+    /*If the text become too long 'forget' the oldest lines*/
+    else if(old_len + txt_len > MAX_TEXT_BUFFER_SZ) {
         uint16_t new_start;
-        for(new_start = 0; new_start < prv_buffer_len; new_start++) {
+        for(new_start = 0; new_start < old_len; new_start++) {
             if(textBuffer[new_start] == '\n') {
-                // Is there enough space for the new text?
-                if(new_start >= input_txt_len) {
-                    // Wait until a non-newline/return char. is found
+                /*If there is enough space break*/
+                if(new_start >= txt_len) {
+                    /*Ignore line breaks*/
                     while(textBuffer[new_start] == '\n' || textBuffer[new_start] == '\r') new_start++;
-                    // Enough space has been made in the buffer
                     break;
                 }
             }
         }
 
-        // There wasn't enough space to make room for the new characters
-        if(new_start == prv_buffer_len)
-            new_start = prv_buffer_len - (512 - input_txt_len); // discard old text in favor of input text
-        
-        // Move the new text into the beginning of the buffer
-        for(uint16_t j = new_start; j < prv_buffer_len; j++) {
-            // Copy each character to the buffer and shift it to the beginning
+        /* If it wasn't able to make enough space on line breaks
+         * simply forget the oldest characters*/
+        if(new_start == old_len) {
+            new_start = old_len - (MAX_TEXT_BUFFER_SZ - txt_len);
+        }
+        /*Move the remaining text to the beginning*/
+        uint16_t j;
+        for(j = new_start; j < old_len; j++) {
             textBuffer[j - new_start] = textBuffer[j];
         }
-        prv_buffer_len = prv_buffer_len - new_start;
-        textBuffer[prv_buffer_len] = '\0';
+        old_len = old_len - new_start;
+        textBuffer[old_len] = '\0';
+
     }
 
-    // Update textBuffer with the new contents
-    memcpy(&textBuffer[prv_buffer_len], txt_in, input_txt_len);
-    // Add a '\0' to the end of the textBuffer
-    textBuffer[prv_buffer_len + input_txt_len] = '\0';
+    memcpy(&textBuffer[old_len], txt_in, txt_len);
+    textBuffer[old_len + txt_len] = '\0';
+    //lv_win_focus(win, clr_btn, TERMINAL_ANIM_TIME);
 }
 
 
@@ -114,13 +111,13 @@ void load_task() {
   lv_canvas_set_buffer(canvas, buffer, 240, 25, LV_IMG_CF_TRUE_COLOR);
   lv_canvas_fill_bg(canvas, lv_color_black(), LV_OPA_COVER);
   lv_draw_rect_dsc_t rect_dsc;
-  lv_draw_rect_dsc_init(&rect_dsc);
   rect_dsc.bg_color = lv_palette_main(LV_PALETTE_GREY);
+  rect_dsc.bg_opa = LV_OPA_COVER;
+  lv_draw_rect_dsc_init(&rect_dsc);
   lv_canvas_draw_rect(canvas, 0, 0, 240, 25, &rect_dsc);
 
-  // TODO
-  // Add an icon showing wiFi RSSI levels
-  // Timer to check if we are still connected to WiFi levels every 2000ms
+  // WiFi Icon
+  // TODO: Timer to check if we are still connected to WiFi levels every 2000ms
   lv_timer_t * timer = lv_timer_create(cb_check_wifi_connection, 2000, NULL);
   lv_obj_t * statusbar_icon_wifi = lv_label_create(lv_scr_act());
   lv_label_set_text(statusbar_icon_wifi, SYMBOL_WIFI_30PX);
@@ -141,8 +138,8 @@ void load_task() {
   // Add a label to hold console text
   // TODO: Speed this up via https://docs.lvgl.io/8.2/widgets/core/label.html#very-long-texts
   consoleLabel = lv_label_create(lv_scr_act());
-  lv_obj_align(consoleLabel, LV_ALIGN_BOTTOM_LEFT, 5, 0);
-  lv_label_set_long_mode(consoleLabel, LV_LABEL_LONG_WRAP);
+  lv_obj_align(consoleLabel, LV_ALIGN_BOTTOM_LEFT, 5, -5);
+  //lv_label_set_long_mode(consoleLabel, LV_LABEL_LONG_WRAP);
   lv_obj_set_width(consoleLabel, 240);
   
   static lv_style_t styleConsoleLabel;
@@ -150,7 +147,7 @@ void load_task() {
   lv_style_set_text_color(&styleConsoleLabel, lv_color_white());
   lv_obj_add_style(consoleLabel, &styleConsoleLabel, LV_PART_MAIN);
   lv_label_set_text_static(consoleLabel, textBuffer);
-  lv_timer_t * timer_cb_console = lv_timer_create(cb_add_to_console, 2500,  NULL);
+  lv_timer_t * timer_cb_console = lv_timer_create(cb_add_to_console, 500,  NULL);
 
 
 /*   labelConsoleText = lv_label_create(lv_scr_act());
