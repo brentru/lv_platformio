d119abb5e6a45621abea3c668393cd953b325b3f
brentru <brent@adafruit.com>
2023-05-31 15:28:38 -0400

    prointf

--- src/main.c
+++ src/main.c
@@ -31,41 +31,43 @@ static char textBuffer[MAX_TEXT_BUFFER_SZ + 1]; // + '\0'
 
 #define DISPLAY_WIDTH 240
 
-int i = 0;
+static char txtBuffer[256];
+
+float i = 0.0;
 
 // Adds data to the monitor
 // Referenced: https://github.com/lvgl/lv_demos/blob/release/v6/lv_apps/terminal/terminal.c
 void monitor_add(const char * txt_in)
 {
+    // Calculate text size
     size_t txt_len = strlen(txt_in);
     size_t old_len = strlen(textBuffer);
 
-    /*If the data is longer then the terminal ax size show the last part of data*/
+    // If the data is longer then the terminal ax size show the last part of data
     if(txt_len > MAX_TEXT_BUFFER_SZ) {
         txt_in += (txt_len - MAX_TEXT_BUFFER_SZ);
         txt_len = MAX_TEXT_BUFFER_SZ;
         old_len = 0;
     }
-    /*If the text become too long 'forget' the oldest lines*/
+
+    // If the text become too long 'forget' the oldest lines
     else if(old_len + txt_len > MAX_TEXT_BUFFER_SZ) {
         uint16_t new_start;
         for(new_start = 0; new_start < old_len; new_start++) {
             if(textBuffer[new_start] == '\n') {
-                /*If there is enough space break*/
                 if(new_start >= txt_len) {
-                    /*Ignore line breaks*/
                     while(textBuffer[new_start] == '\n' || textBuffer[new_start] == '\r') new_start++;
                     break;
                 }
             }
         }
 
-        /* If it wasn't able to make enough space on line breaks
-         * simply forget the oldest characters*/
+        // If it wasn't able to make enough space on line breaks simply forget the oldest characters
         if(new_start == old_len) {
             new_start = old_len - (MAX_TEXT_BUFFER_SZ - txt_len);
         }
-        /*Move the remaining text to the beginning*/
+
+        // Move the remaining text to the beginning
         uint16_t j;
         for(j = new_start; j < old_len; j++) {
             textBuffer[j - new_start] = textBuffer[j];
@@ -75,20 +77,49 @@ void monitor_add(const char * txt_in)
 
     }
 
+    // Copy new text to the text buffer
     memcpy(&textBuffer[old_len], txt_in, txt_len);
     textBuffer[old_len + txt_len] = '\0';
+
+    // Update label
+    lv_label_set_text_static(consoleLabel, textBuffer);
+}
+
+//  Used for sending text to the display
+/* void add_to_console(char *text, const char *format, bool value) {
+  snprintf(txtBuffer, 256, text, format, value);
+  monitor_add(txtBuffer);
+}
+
+void add_to_console(char *text, const char *format, int value) {
+  snprintf(txtBuffer, 256, text, format, value);
+  monitor_add(txtBuffer);
+} */
+
+void add_to_console_f(char *text, float value) {
+  snprintf(txtBuffer, 256, text, value);
+  monitor_add(txtBuffer);
+}
+
+
+// Value from sensor
+void add_to_console_i2c(char *text, int sensor_address, float value) {
+  snprintf(txtBuffer, 256, text, sensor_address, value);
+  monitor_add(txtBuffer);
 }
 
 void cb_add_to_console(lv_timer_t * timer) {
-  // append to the buffer
-  static char txt[340];
-  snprintf(txt, 340, "%d - test\n", i);
-  monitor_add(txt);
-  // set label
-  lv_label_set_text_static(consoleLabel, textBuffer);
-  
-  i++;
+  // TODO: Randomize this?
+  printf("add_to_console");
+  //add_to_console_f("Sending Pin A2 Value: %0.2f\n", i);
+  add_to_console_i2c("Sensor 0x%d Value: %0.2f\n", 0x48, i);
+
+  i+=0.5;
 }
+
+
+
+
 
 void load_task() {
   printf("in load_task\n");
@@ -144,7 +175,7 @@ void load_task() {
   lv_obj_add_style(consoleLabel, &styleConsoleLabel, LV_PART_MAIN);
   lv_label_set_text_static(consoleLabel, textBuffer);
   lv_obj_move_background(consoleLabel);
-  lv_timer_t * timer_cb_console = lv_timer_create(cb_add_to_console, 500,  NULL);
+  lv_timer_t * timer_cb_console = lv_timer_create(cb_add_to_console, 300,  NULL);
 
 }
 
@@ -156,6 +187,8 @@ int main(void) {
 
   printf("call load_task\n");
   load_task();
+
+  lv_event_send(consoleLabel, LV_EVENT_REFRESH, "data");
 
   // printf("task handler\n");
   // lv_task_handler();
